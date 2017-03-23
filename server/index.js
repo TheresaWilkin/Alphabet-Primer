7fb40ee2a4211aed95b6e40839f04430902bc946
Theresa Wilkin <theresawilkin@yahoo.com>
2017-03-23 17:40:03 -0400

    google auth

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://powerful-depths-44928.herokuapp.com/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
     getArrayOfQuestions(profile.id)
